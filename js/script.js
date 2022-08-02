17025427cf818f10c7e19189b1072463273dde8e
ryosuke-shibata-hub <ibis.stk@gmail.com>
2022-08-02 23:47:05 +0900

    20220802

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
-$(function () {
-    var counter = 0;
-    function doAction() {
-        $('#msg').text("clicked: " + ++counter);
-    }
-})
+// $(function () {
+
+//     const c_arr = ['primary', 'secoundary', 'info', 'warning', 'danger'];
+//     for (var i = 0; i < 10; i++) {
+//         $('#btn_container').append($('<button>')
+//             .attr('class', 'btn m-1 btn-' + c_arr[i % 5])
+//             .text('Button No,' + (i + 1)));
+//     }
+//     for (var i = 0; i < 10; i++) {
+//         var obj = $('<button>')
+//             .attr('class', 'btn m-1 bg-info')
+//             .text('Button No,' + (i + 1));
+//         if (i % 3 == 1) {
+//             obj.addClass('active');
+//         } else if (i % 3 == 2) {
+//             obj.addClass('disabled');
+//         }
+//         $('#btn_container_2').append(obj);
+//     }
+
+//     $('#ck1').prop('checked', true);
+//     $('#ck2').prop('indeterminate', true);
+
+//  })();
